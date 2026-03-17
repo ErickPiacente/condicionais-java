975d99a23a58528412bd1b3e5e170be40370a751
Erick <erickhenriquepiacente@gmail.com>
2026-03-17 08:55:03 -0300

    Resolvendo exercicios

--- Ex015.java
+++ Ex015.java
@@ -8,9 +8,8 @@ public class Ex015 {
     String usuarioLogin = leia.next();
     System.out.println("Digite sua senha: ");
     int senhaLogin = leia.nextInt();
-    String resultado = (usuarioLogin == usuario && senhaLogin == senha)? "Acesso aprovado":"Acesso negado";
+    String resultado = (usuarioLogin.equals(usuario) && senhaLogin == senha)? "Acesso aprovado":"Acesso negado";
     System.out.println(resultado);
     leia.close();
-    // Professor, eu tentei resolver o problema desse exercicio, mas não consigo achar o erro então esse aqui estarei mandando errado mesmo 
  }   
 }
--- Ex019.java
+++ Ex019.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class Ex019 {
-    //Esse aqui eu tentei fazer mas eu não consegui achar uma solução, espero aprender depois
              public static void main(String[] args){
     Scanner leia = new Scanner(System.in);
     System.out.println("Informe a sua velocidade: ");
     int velocidade = leia.nextInt();
     System.out.println("Informe o limite da via: ");
     int limite = leia.nextInt();
-    String resultado = (velocidade <= limite) ? "Está sem multa": (velocidade % 200 == 0  ) ? "Multa leve": "Multa media";
+    String resultado = (velocidade <= limite) ? "Está sem multa":
+    (velocidade <= limite + (limite*0.20)  ) ? "Multa leve":
+    (velocidade <= limite + (limite*0.50)) ? "Multa media":
+    "Multa grave";
     System.out.println(resultado);
     leia.close();
          }
