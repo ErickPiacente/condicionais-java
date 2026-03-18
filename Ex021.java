712e0e57e7fc380ae066274be2421b68ee7b78be
unknown <erickhenriquepiacente@gmail.com>
2026-03-18 08:48:48 -0300

    Criando o arquivo do teatro

---
+++ Ex021.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Ex021{
+   public static void main(String[] args){
+    Scanner leia = new Scanner(System.in);
+    System.out.println("Olá, bem vindo ao Devisate aqui temos varias salas com muitos lugares")
+
+
+   }
+
+
+}
