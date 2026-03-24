0978092613e7df5844f507bcabdd1bd58d7c969c
Erick <erickhenriquepiacente@gmail.com>
2026-03-24 08:43:27 -0300

    Consertando

--- Ex021.java
+++ Ex021.java
@@ -3,18 +3,11 @@ import java.util.Scanner;
 public class Ex021{
    public static void main(String[] args){
     Scanner leia = new Scanner(System.in);
-    System.out.println("----------------MENU DE SALAS----------------");
-    System.out.println("Sala 1-- Sala Cientifica");
-    System.out.println("Sala 2-- Sala de Organização documental");
-    System.out.println("Sala 3-- Sala de tecnologia");
-    System.out.println("Sala 4-- Sala de testes quimicos");
-    System.out.println("---------------------------------------------");
-    System.out.println("Escolha qual sala você quer entrar(Escolha pelo numero): ");
-    int opcao = leia.nextInt();
-    
-    switch (opcao) {
-      case 1:
-          System.out.println("Sala 1-- Sala Cientifica");
+    System.out.println("Escreva o nome da sala que você quer criar: ");
+    String sala = leia.nextLine();
+
+
+          System.out.println("Sala "+sala);
 
                  int[][] matriz = new int[12][12];
 
@@ -27,8 +20,7 @@ public class Ex021{
             System.out.println();
 
         }
-        break;
-    }
+
 
 
     }
