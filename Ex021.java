5d0797ccd7aa16376e7803981550175288e47a4c
ErickPiacente <erickhenriquepiacente@gmail.com>
2026-03-25 19:36:40 -0300

    Corrigindo o prencher o vetor e imprimir o vetor

--- Ex021.java
+++ Ex021.java
@@ -1,8 +1,15 @@
 import java.util.Scanner;
 import java.util.Arrays;
 public class Ex021{
-   public static void main(String[] args){
-    
+   public static void main(String[] args){ 
+         char[][][] matriz = new char[8][12][12];
+      for(int m = 0; m < matriz.length; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+            }}}
     Scanner leia = new Scanner(System.in);
         System.out.println("----------------MENU DE SALAS----------------");
     System.out.println("Sala 1-- Sala Cientifica");
@@ -16,32 +23,25 @@ public class Ex021{
     System.out.println("---------------------------------------------");
     System.out.println("Escolha qual sala você quer entrar(Escolha pelo numero): ");
     int opcao = leia.nextInt();
-    char[][][] matriz = new char[8][12][12];
+
     switch (opcao) {
       case 1:
 int enquanto;
-int opcao2;
+
 do{ 
 
           System.out.println("Sala 1-- Sala Cientifica");
           System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
 
-        for (int i = 0; i < matriz[m].length; i++) {
+
+        for (int i = 0; i < matriz[0].length; i++) {
             System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                System.out.println("\t MENU DE CADEIRAS");
-                System.out.println("\t  1--Reservar uma cadeira");
-                System.out.println("\t  2--Comprar uma cadeira");
-                System.out.println("Escolha entre 1 e 2");
-                opcao2 = leia.nextInt();
-                switch(opcao2){
-                    case 1:
-                        System.out.println("Mostre");
-                }
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            for (int j = 0; j < matriz[0][i].length; j++) {
+
+
+
+                System.out.print("["+matriz[0][i][j]+"]" + "   ");
+
 
             }
             System.out.println();
@@ -50,178 +50,19 @@ do{
 
 
         }
-    }
+        System.out.println("Informe a linha da cadeira");
+        char linha = leia.next().charAt(0);
+        System.out.println("Informe a coluna da cadeira");
+        int coluna = leia.nextInt();
+        linha = (char)(linha - 'A');
+        coluna = coluna - 1;
+
+            
            System.out.println("Quer continuar o programa? digite 1 se quiser continuar;\nSe quiser sair digite qualquer numero: ");
         enquanto = leia.nextInt();
     }while(enquanto == 1);
     break;
-              case 2:
-        
 
-          System.out.println("Sala 2-- Sala de TCC");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
-
-        for (int i = 0; i < matriz[m].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
-            }
-            System.out.println();
-            
-
-
-
-        }
-    }
-    break;
-          case 3:
-        
-
-          System.out.println("Sala 3-- Sala de apresentações");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
-
-        for (int i = 0; i < matriz[m].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
-            }
-            System.out.println();
-            
-
-
-
-        }
-    }
-    break;
-          case 4:
-        
-
-          System.out.println("Sala 4-- Sala de monitoria educacional");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
-
-        for (int i = 0; i < matriz[m].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
-            }
-            System.out.println();
-            
-
-
-
-        }
-    }
-    break;
-          case 5:
-        
-
-          System.out.println("Sala 5-- Sala facultativa");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
-
-        for (int i = 0; i < matriz[m].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
-            }
-            System.out.println();
-            
-
-
-
-        }
-    }
-    break;
-              case 6:
-        
-
-          System.out.println("Sala 6-- Sala de debate politico");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
-
-        for (int i = 0; i < matriz[m].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
-            }
-            System.out.println();
-            
-
-
-
-        }
-    }
-    break;
-          case 7:
-        
-
-          System.out.println("Sala 7-- Sala Devisate");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
-
-        for (int i = 0; i < matriz[m].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
-            }
-            System.out.println();
-            
-
-
-
-        }
-    }
-    break;
-          case 8:
-        
-
-          System.out.println("Sala 8-- Sala de provas");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
-  for(int m = 0; m < 1; m++){
-
-        for (int i = 0; i < matriz[m].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[m][i].length; j++) {
-                matriz[m][i][j] = 'L';
-                
-                
-                System.out.print("["+matriz[m][i][j]+"]" + "   ");
-            }
-            System.out.println();
-            
-
-
-
-        }
-    }
-    break;
-    default:
-        System.out.println("Você digitou uma sala que não existe, digite dentre as opcões do menu.");
-    }
-
-
-                 
 
 
 
@@ -235,7 +76,7 @@ do{
 
     
 
-    
+}   
 
 }
 
