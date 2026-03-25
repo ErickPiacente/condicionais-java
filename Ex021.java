e8674fdabb90459ea0154d270f1980929220a82f
ErickPiacente <erickhenriquepiacente@gmail.com>
2026-03-25 15:09:49 -0300

    Colocando laço de repetição e saida de sala/progrma

--- Ex021.java
+++ Ex021.java
@@ -16,10 +16,12 @@ public class Ex021{
     System.out.println("---------------------------------------------");
     System.out.println("Escolha qual sala você quer entrar(Escolha pelo numero): ");
     int opcao = leia.nextInt();
-    char[][][] matriz = new char[4][12][12];
+    char[][][] matriz = new char[8][12][12];
     switch (opcao) {
       case 1:
-        
+int enquanto;
+int opcao2;
+do{ 
 
           System.out.println("Sala 1-- Sala Cientifica");
           System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
@@ -29,9 +31,18 @@ public class Ex021{
             System.out.print((char) ('A' + i)+"  ");
             for (int j = 0; j < matriz[m][i].length; j++) {
                 matriz[m][i][j] = 'L';
-                
+                System.out.println("\t MENU DE CADEIRAS");
+                System.out.println("\t  1--Reservar uma cadeira");
+                System.out.println("\t  2--Comprar uma cadeira");
+                System.out.println("Escolha entre 1 e 2");
+                opcao2 = leia.nextInt();
+                switch(opcao2){
+                    case 1:
+                        System.out.println("Mostre");
+                }
                 
                 System.out.print("["+matriz[m][i][j]+"]" + "   ");
+
             }
             System.out.println();
             
@@ -40,6 +51,9 @@ public class Ex021{
 
         }
     }
+           System.out.println("Quer continuar o programa? digite 1 se quiser continuar;\nSe quiser sair digite qualquer numero: ");
+        enquanto = leia.nextInt();
+    }while(enquanto == 1);
     break;
               case 2:
         
