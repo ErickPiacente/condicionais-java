5b7c1b80d618c41693bfe24b4110cf4e0b47b07b
Erick <erickhenriquepiacente@gmail.com>
2026-03-25 08:42:41 -0300

    Copiando e colando as outras salas

--- Ex021.java
+++ Ex021.java
@@ -6,9 +6,13 @@ public class Ex021{
     Scanner leia = new Scanner(System.in);
         System.out.println("----------------MENU DE SALAS----------------");
     System.out.println("Sala 1-- Sala Cientifica");
-    System.out.println("Sala 2-- Sala de Organização documental");
-    System.out.println("Sala 3-- Sala de tecnologia");
-    System.out.println("Sala 4-- Sala de testes quimicos");
+    System.out.println("Sala 2-- Sala de TCC");
+    System.out.println("Sala 3-- Sala de apresentações");
+    System.out.println("Sala 4-- Sala de monitoria educacional");
+    System.out.println("Sala 5-- Sala facultativa");
+    System.out.println("Sala 6-- Sala de debate politico");
+    System.out.println("Sala 7-- Sala Devisate");
+    System.out.println("Sala 8-- Sala de provas");
     System.out.println("---------------------------------------------");
     System.out.println("Escolha qual sala você quer entrar(Escolha pelo numero): ");
     int opcao = leia.nextInt();
@@ -18,7 +22,7 @@ public class Ex021{
         
 
           System.out.println("Sala 1-- Sala Cientifica");
-          System.out.println(" 1     2     3     4     5     6     7     8     9    10    11    12");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
   for(int m = 0; m < 1; m++){
 
         for (int i = 0; i < matriz[m].length; i++) {
@@ -30,9 +34,176 @@ public class Ex021{
                 System.out.print("["+matriz[m][i][j]+"]" + "   ");
             }
             System.out.println();
+            
+
+
 
         }
     }
+    break;
+              case 2:
+        
+
+          System.out.println("Sala 2-- Sala de TCC");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+  for(int m = 0; m < 1; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+            System.out.print((char) ('A' + i)+"  ");
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+                
+                
+                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            }
+            System.out.println();
+            
+
+
+
+        }
+    }
+    break;
+          case 3:
+        
+
+          System.out.println("Sala 3-- Sala de apresentações");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+  for(int m = 0; m < 1; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+            System.out.print((char) ('A' + i)+"  ");
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+                
+                
+                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            }
+            System.out.println();
+            
+
+
+
+        }
+    }
+    break;
+          case 4:
+        
+
+          System.out.println("Sala 4-- Sala de monitoria educacional");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+  for(int m = 0; m < 1; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+            System.out.print((char) ('A' + i)+"  ");
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+                
+                
+                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            }
+            System.out.println();
+            
+
+
+
+        }
+    }
+    break;
+          case 5:
+        
+
+          System.out.println("Sala 5-- Sala facultativa");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+  for(int m = 0; m < 1; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+            System.out.print((char) ('A' + i)+"  ");
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+                
+                
+                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            }
+            System.out.println();
+            
+
+
+
+        }
+    }
+    break;
+              case 6:
+        
+
+          System.out.println("Sala 6-- Sala de debate politico");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+  for(int m = 0; m < 1; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+            System.out.print((char) ('A' + i)+"  ");
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+                
+                
+                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            }
+            System.out.println();
+            
+
+
+
+        }
+    }
+    break;
+          case 7:
+        
+
+          System.out.println("Sala 7-- Sala Devisate");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+  for(int m = 0; m < 1; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+            System.out.print((char) ('A' + i)+"  ");
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+                
+                
+                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            }
+            System.out.println();
+            
+
+
+
+        }
+    }
+    break;
+          case 8:
+        
+
+          System.out.println("Sala 8-- Sala de provas");
+          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+  for(int m = 0; m < 1; m++){
+
+        for (int i = 0; i < matriz[m].length; i++) {
+            System.out.print((char) ('A' + i)+"  ");
+            for (int j = 0; j < matriz[m][i].length; j++) {
+                matriz[m][i][j] = 'L';
+                
+                
+                System.out.print("["+matriz[m][i][j]+"]" + "   ");
+            }
+            System.out.println();
+            
+
+
+
+        }
+    }
+    break;
+    default:
+        System.out.println("Você digitou uma sala que não existe, digite dentre as opcões do menu.");
     }
 
 
