0e18f6574229cbae5fdc63122c071822f375d2ed
ErickPiacente <erickhenriquepiacente@gmail.com>
2026-03-28 14:45:15 -0300

    Trabalho quase finalizado

--- Ex021.java
+++ Ex021.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 public class Ex021{
    public static void main(String[] args){ 
          char[][][] matriz = new char[8][12][12];
@@ -12,39 +11,83 @@ public class Ex021{
             }}}
     Scanner leia = new Scanner(System.in);
         System.out.println("----------------MENU DE SALAS----------------");
-    System.out.println("Sala 1-- Sala Cientifica");
-    System.out.println("Sala 2-- Sala de TCC");
-    System.out.println("Sala 3-- Sala de apresentações");
-    System.out.println("Sala 4-- Sala de monitoria educacional");
-    System.out.println("Sala 5-- Sala facultativa");
-    System.out.println("Sala 6-- Sala de debate politico");
-    System.out.println("Sala 7-- Sala Devisate");
-    System.out.println("Sala 8-- Sala de provas");
+System.out.println("1 - Sala Científica | Espetáculo: Viagem Pelo Universo");
+System.out.println("2 - Sala Acadêmica | Espetáculo: Apresentação de projetos");
+System.out.println("3 - Sala de Apresentações | Espetáculo: Show de Talentos");
+System.out.println("4 - Sala Educacional | Espetáculo: História da Tecnologia");  //Usei IA para gerar os nomes e espetaculos da sala
+System.out.println("5 - Sala Cultural | Espetáculo: Festival de Dança");
+System.out.println("6 - Sala de Debates | Espetáculo: O Futuro do Brasil");
+System.out.println("7 - Sala Mundial | Espetáculo: Ideias que Mudam o Mundo");
+System.out.println("8 - Sala de Provas | Espetáculo: Resultado das provas");        
     System.out.println("---------------------------------------------");
     System.out.println("Escolha qual sala você quer entrar(Escolha pelo numero): ");
     int opcao = leia.nextInt();
 
-    switch (opcao) {
-      case 1:
+
+
 int enquanto;
 
-
 do{ 
+
 int contador = 0;
 int contadorReserva = 0;
 int contadorComprado = 0;
+Double totalCompra = 0.00;
+Double totalReserva = 0.00;
+Double valorIngresso = 20.00;
+Double valorPotencial = 0.00;
+    switch(opcao){
+         case 1:
+         System.out.println("Sala 1-- Sala Científica ");
+         System.out.println("Espetaculo: Viagem Pelo Universo");
+         break;
+         case 2:
+         System.out.println("Sala 2-- Sala Acadêmica");
+         System.out.println("Espetáculo: Apresentação de projetos");
+         break;
+         case 3:
+         System.out.println("Sala 3-- Sala de Apresentações");
+         System.out.println("Espetáculo: Show de talentos");
+         break;
+         case 4:
+         System.out.println("Sala 4-- Sala educacional");
+         System.out.println("Espetáculo: História da Tecnologia");
+         break;
+         case 5:
+         System.out.println("Sala 5-- Sala cultural");
+         System.out.println("Espetáculo: Festival de Dança");
+         break;
+         case 6:
+         System.out.println("Sala 6-- Sala de debates");
+         System.out.println("Espetáculo: O futuro do Brasil");
+         break;
+         case 7:
+         System.out.println("Sala 7-- Sala mundial");
+         System.out.println("Espetáculo: Ideias que mudam o mundo");
+         break;
+         case 8:
+         System.out.println("Sala 8-- Sala de provas");
+         System.out.println("Espetáculo: Resultado das provas");
+         break;
+         default:
+            System.out.println("A sala digitada não existe!");
+         return;
+         
+               
 
-          System.out.println("Sala 1-- Sala Cientifica");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+    }
+
+          System.out.println();
+          System.out.println("    1     2     3     4     5     6     7     8     9    10    11    12");
 
 
-        for (int i = 0; i < matriz[0].length; i++) {
+        for (int i = 0; i < matriz[opcao - 1].length; i++) {
             System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[0][i].length; j++) {
+            for (int j = 0; j < matriz[opcao - 1][i].length; j++) {
 
 
 
-                System.out.print("["+matriz[0][i][j]+"]" + "   ");
+                System.out.print("["+matriz[opcao - 1][i][j]+"]" + "   ");
 
 
             }
@@ -54,43 +97,55 @@ int contadorComprado = 0;
 
 
         }
-        for(int i = 0; i < matriz[0].length; i++){
-            for(int j = 0; j < matriz[0][i].length; j++){
-                if(matriz[0][i][j] == 'L'){
+        for(int i = 0; i < matriz[opcao - 1].length; i++){
+            for(int j = 0; j < matriz[opcao - 1][i].length; j++){
+                if(matriz[opcao - 1][i][j] == 'L'){
                     contador++;
+                    valorPotencial = valorPotencial + valorIngresso;
+
                 }
 
             }
         }
     System.out.print("Cadeiras livres: "+ contador);
 
-        for(int i = 0; i < matriz[0].length; i++){
-            for(int j = 0; j < matriz[0][i].length; j++){
-                if(matriz[0][i][j] == 'R'){
+        for(int i = 0; i < matriz[opcao - 1].length; i++){
+            for(int j = 0; j < matriz[opcao - 1][i].length; j++){
+                if(matriz[opcao - 1][i][j] == 'R'){
                     contadorReserva++;
+                    totalReserva = totalReserva + valorIngresso;
+
                 }
 
             }
         }
     System.out.print("\tCadeiras reservadas: "+ contadorReserva);
+    
 
-        for(int i = 0; i < matriz[0].length; i++){
-            for(int j = 0; j < matriz[0][i].length; j++){
-                if(matriz[0][i][j] == 'C'){
+        for(int i = 0; i < matriz[opcao - 1].length; i++){
+            for(int j = 0; j < matriz[opcao - 1][i].length; j++){
+                if(matriz[opcao - 1][i][j] == 'C'){
                     contadorComprado++;
+                    totalCompra = totalCompra + valorIngresso;
+                    
                 }
 
             }
         }
-    System.out.println("\tCadeiras compradas: "+ contadorComprado);
+    System.out.print("\tCadeiras compradas: "+ contadorComprado);
+    System.out.print("\tTotal arrecadado: "+ totalCompra);
+    System.out.println("\tTotal de reservas pagas: "+ (totalReserva / 2));
+    System.out.println("Total arrecadado: "+ valorPotencial);
 
         System.out.println("MENU DE OPERAÇÔES!");
         System.out.println("1--Reservar cadeira");
         System.out.println("2--Comprar cadeira");
         System.out.println("3--Cancelar reserva");
+        System.out.println("4--Mudar de sala");
+        System.out.println("Outro numero--Termina o programa");
         System.out.println("Informe se quer reservar ou comprar uma cadeira");
         int opcao2 = leia.nextInt();
-        int valorIngresso = 20;
+        
         switch(opcao2){
             case 1:
         System.out.println("Você escolheu reservar uma cadeira");        
@@ -103,20 +158,20 @@ int contadorComprado = 0;
 
 
         coluna = coluna - 1;
-        if(linhaInt <= 12 && coluna <= 12 || linhaInt >= 1 && coluna >= 1){
-        if(matriz[0][linhaInt][coluna] == 'L'){
-        matriz[0][linhaInt][coluna] = 'R';
-                  System.out.println("Sala 1-- Sala Cientifica");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+        if(linhaInt <= 11 && coluna <= 11 && linhaInt >= 0 && coluna >= 0){
+        if(matriz[opcao - 1][linhaInt][coluna] == 'L'){
+        matriz[opcao - 1][linhaInt][coluna] = 'R';
+
+         System.out.println("    1     2     3     4     5     6     7     8     9    10    11    12");
 
 
-        for (int i = 0; i < matriz[0].length; i++) {
+        for (int i = 0; i < matriz[opcao - 1].length; i++) {
             System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[0][i].length; j++) {
+            for (int j = 0; j < matriz[opcao - 1][i].length; j++) {
 
 
          
-                System.out.print("["+matriz[0][i][j]+"]" + "   ");
+                System.out.print("["+matriz[opcao - 1][i][j]+"]" + "   ");
 
 
             }
@@ -129,10 +184,15 @@ int contadorComprado = 0;
         }
         System.out.println("O valor pago pela reserva é de "+ (valorIngresso - 10));
     }
-}
     else{
         System.out.println("O lugar já está reservado/ocupado, por favor escolha outro");
     }
+}
+else{
+        System.out.println("O lugar digitado não existe, por favor tente outro");
+  
+}
+    
     break;
     case 2:
         System.out.println("Você escolheu comprar uma cadeira");
@@ -142,23 +202,23 @@ int contadorComprado = 0;
         coluna = leia.nextInt();
         linhaInt = linha - 'A';
         linhaInt = (linha - 'A');
-
-
         coluna = coluna - 1;
-        if(linhaInt <= 12 && coluna <= 12 || linhaInt >= 1 && coluna >= 1){
-        if(matriz[0][linhaInt][coluna] == 'L'){
-        matriz[0][linhaInt][coluna] = 'C';
-                  System.out.println("Sala 1-- Sala Cientifica");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
 
 
-        for (int i = 0; i < matriz[0].length; i++) {
+        if(linhaInt <= 11 && coluna <= 11 && linhaInt >= 0 && coluna >= 0){
+        if(matriz[opcao - 1][linhaInt][coluna] == 'L' || matriz[opcao - 1][linhaInt][coluna] == 'R'){
+        matriz[opcao - 1][linhaInt][coluna] = 'C';
+
+          System.out.println("    1     2     3     4     5     6     7     8     9    10    11    12");
+
+
+        for (int i = 0; i < matriz[opcao - 1].length; i++) {
             System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[0][i].length; j++) {
+            for (int j = 0; j < matriz[opcao - 1][i].length; j++) {
 
 
             
-                System.out.print("["+matriz[0][i][j]+"]" + "   ");
+                System.out.print("["+matriz[opcao - 1][i][j]+"]" + "   ");
 
 
             }
@@ -171,10 +231,15 @@ int contadorComprado = 0;
         }
         System.out.println("O valor pago pela compra do lugar é de "+ (valorIngresso));
     }
-}
-    else{
-        System.out.println("O lugar já está reservado/ocupado, por favor escolha outro");
+        else{
+        System.out.println("O lugar já está ocupado, por favor escolha outro");
     }
+}
+else{
+        System.out.println("O lugar digitado não existe, por favor tente outro");
+  
+}
+
 
 
 
@@ -191,20 +256,20 @@ int contadorComprado = 0;
 
 
         coluna = coluna - 1;
-        if(linhaInt <= 12 && coluna <= 12 || linhaInt >= 1 && coluna >= 1){
-        if(matriz[0][linhaInt][coluna] == 'R'){
-        matriz[0][linhaInt][coluna] = 'L';
-                  System.out.println("Sala 1-- Sala Cientifica");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
+        if(linhaInt <= 11 && coluna <= 11 && linhaInt >= 0 && coluna >=0){
+        if(matriz[opcao - 1][linhaInt][coluna] == 'R'){
+        matriz[opcao - 1][linhaInt][coluna] = 'L';
+
+          System.out.println("    1     2     3     4     5     6     7     8     9    10    11    12");
 
 
-        for (int i = 0; i < matriz[0].length; i++) {
+        for (int i = 0; i < matriz[opcao - 1].length; i++) {
             System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[0][i].length; j++) {
+            for (int j = 0; j < matriz[opcao - 1][i].length; j++) {
 
 
          
-                System.out.print("["+matriz[0][i][j]+"]" + "   ");
+                System.out.print("["+matriz[opcao - 1][i][j]+"]" + "   ");
 
 
             }
@@ -217,11 +282,22 @@ int contadorComprado = 0;
         }
         System.out.println("O valor de rembolso é: "+ (valorIngresso - 10));
     }
-}
-    else{
+        else{
         System.out.println("O lugar foi comprado ou está livre, só é possivel reembolsar lugares reservados");
     }
+}
+else{
+        System.out.println("O lugar digitado não existe, por favor tente outro");
+  
+}
+
         break;
+        case 4:
+            System.out.println("Escolha outra sala");
+            opcao = leia.nextInt();
+            break;
+            default:
+                System.out.println("A opção é invalida, programa encerrado");
         }
 
 
@@ -229,37 +305,8 @@ int contadorComprado = 0;
            System.out.println("Quer continuar o programa? digite 1 se quiser continuar;\nSe quiser sair digite qualquer numero: ");
         enquanto = leia.nextInt();
     }while(enquanto == 1);
-    break;
-    case 2:
-    do{ 
-
-          System.out.println("Sala 2-- Sala de TCC");
-          System.out.println("  1      2      3      4      5      6      7      8      9     10     11     12");
 
 
-        for (int i = 0; i < matriz[1].length; i++) {
-            System.out.print((char) ('A' + i)+"  ");
-            for (int j = 0; j < matriz[1][i].length; j++) {
-
-
-
-                System.out.print("["+matriz[1][i][j]+"]" + "   ");
-
-
-            }
-            System.out.println();
-            
-
-
-
-        }
-
-
-            
-           System.out.println("Quer continuar o programa? digite 1 se quiser continuar;\nSe quiser sair digite qualquer numero: ");
-        enquanto = leia.nextInt();
-    }while(enquanto == 1);
-    break;
 
 
 
@@ -276,7 +323,7 @@ int contadorComprado = 0;
 
 }   
 
-}
+
 
 
 
